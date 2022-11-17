9ad28924375830cbbca8f08a8aa1fd864a0d4276
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-11-17 18:42:19 +0400

    firebase setup

--- Signal-Clone/firebase.js
+++ Signal-Clone/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyD_CIC9hrk1XKpUB6bMLW5GkQry_555omo",
   authDomain: "signal-clone-580f0.firebaseapp.com",
@@ -6,3 +10,15 @@ const firebaseConfig = {
   messagingSenderId: "404611971041",
   appId: "1:404611971041:web:8eddd422e15100a94684a0",
 };
+
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
