e5fa17bd90dbc7ec4718008d9d205240cf4f83f9
Wayne Celestin <waynecelestin.a3@gmail.com>
2022-11-17 17:26:48 +0400

    login screen built!

---
+++ Signal-Clone/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_CIC9hrk1XKpUB6bMLW5GkQry_555omo",
+  authDomain: "signal-clone-580f0.firebaseapp.com",
+  projectId: "signal-clone-580f0",
+  storageBucket: "signal-clone-580f0.appspot.com",
+  messagingSenderId: "404611971041",
+  appId: "1:404611971041:web:8eddd422e15100a94684a0",
+};
---
+++ Signal-Clone/screens/LoginScreen.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { KeyboardAvoidingView } from "react-native";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://logos-download.com/wp-content/uploads/2020/06/Signal_Logo.png",
+        }}
+        style={{ width: 150, height: 150, borderRadius: 15, margin: 15 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} title="Login" onPress={signIn} />
+      <Button containerStyle={styles.button} title="Register" type="outlined" />
+      <View style={{ height: 100 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
